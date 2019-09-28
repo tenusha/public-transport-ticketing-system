ac024bbe54bba94150257a9a1617dacde36520b1
vimukthi <tenushamadhushan@gmail.com>
2019-09-28 09:53:02 +0530

    admin

---
+++ admin/src/App.js
@@ -0,0 +1,108 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component, Suspense } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
+import AccountSettings from './components/AccountSettings'
+import NavBar from './components/commons/NavBar'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      showLogin: false,
+      showRegister: false
+    }
+
+    this.config = {
+      selected: 'home'
+    }
+
+    this.baseState = this.state
+  }
+
+  handleChange = obj => {
+    if (obj instanceof Object) {
+      this.setState({ ...obj })
+    }
+  }
+
+  handleLogout = () => {
+    this.setState(this.baseState)
+    localStorage.clear()
+  }
+
+  handleLoginShow = () => {
+    this.setState({ showLogin: true })
+  }
+
+  handleLoginClose = () => {
+    this.setState({ showLogin: false })
+  }
+
+  handleRegisterShow = () => {
+    this.setState({ showRegister: true })
+  }
+
+  handleRegisterClose = () => {
+    this.setState({ showRegister: false })
+  }
+
+  render() {
+
+    return (
+      <>
+        <div className="main-container">
+          <NavBar
+            handleLoginShow={this.handleLoginShow}
+            handleRegisterShow={this.handleRegisterShow}
+            logout={this.handleLogout}
+            {...this.state}
+          />
+
+          <Login
+            showLogin={this.state.showLogin}
+            handleShow={this.handleLoginShow}
+            handleClose={this.handleLoginClose}
+            handleRegisterShow={this.handleRegisterShow}
+          />
+
+          <Register
+            showRegister={this.state.showRegister}
+            handleShow={this.handleRegisterShow}
+            handleClose={this.handleRegisterClose}
+            handleLoginShow={this.handleLoginShow}
+          />
+
+          <Router>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/admin" component={Home} />
+                <Route exact path="/routs" component={Home} />
+                <Route path="/account" component={AccountSettings} />
+              </Switch>
+            </Suspense>
+          </Router>
+        </div>
+
+        <Footer />
+
+        <ToastContainer
+          autoClose={3000}
+          position="bottom-right"
+        />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ admin/src/components/commons/NavBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+import { Navbar, Nav, NavDropdown, Image, Row } from 'react-bootstrap'
+
+class NavBar extends Component {
+
+    render() {
+        var user = localStorage.getItem('user')
+        if (user) {
+            user = JSON.parse(user)
+        }
+        return (
+            <>
+                <Navbar bg="dark" variant="dark" expand="sm">
+                    <Navbar.Brand href="/">
+                        Railway E-Ticketing Admin Panel
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            {user ?
+                                <>
+
+                                    <Nav.Link href="/" >{'Reports'}</Nav.Link>
+                                    <Nav.Link href="/admin" >{'Admin Management'}</Nav.Link>
+                                    <Nav.Link href="/routs" >{'Routs'}</Nav.Link>
+                                    <NavDropdown title={user.fname} id="nav-dropdown" alignRight>
+                                        <NavDropdown.Item href="/account">Account Settings</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={this.props.logout}>Sign out</NavDropdown.Item>
+                                    </NavDropdown>
+                                </>
+                                :
+                                <>
+                                    <Nav.Link href="" onClick={this.props.handleLoginShow}>Sign In</Nav.Link>
+                                </>
+
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </>
+        );
+    }
+}
+
+export default NavBar;
